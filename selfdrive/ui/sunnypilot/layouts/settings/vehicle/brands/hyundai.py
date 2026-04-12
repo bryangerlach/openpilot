268ee314e29c4fc6896aa558dd17d4bf7fe9141f
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2026-04-12 15:12:30 -0500

    adjustable values

--- selfdrive/ui/sunnypilot/layouts/settings/vehicle/brands/hyundai.py
+++ selfdrive/ui/sunnypilot/layouts/settings/vehicle/brands/hyundai.py
@@ -7,7 +7,7 @@ See the LICENSE.md file in the root directory for more details.
 from openpilot.selfdrive.ui.sunnypilot.layouts.settings.vehicle.brands.base import BrandSettings
 from openpilot.selfdrive.ui.ui_state import ui_state
 from openpilot.system.ui.lib.multilang import tr
-from openpilot.system.ui.sunnypilot.widgets.list_view import multiple_button_item_sp
+from openpilot.system.ui.sunnypilot.widgets.list_view import multiple_button_item_sp, option_item_sp
 from opendbc.car.hyundai.values import CAR, UNSUPPORTED_LONGITUDINAL_CAR
 
 
@@ -20,7 +20,39 @@ class HyundaiSettings(BrandSettings):
     self.longitudinal_tuning_item = multiple_button_item_sp(tr("Custom Longitudinal Tuning"), "", tuning_texts,
                                                             button_width=300, callback=self._on_tuning_selected,
                                                             param="HyundaiLongitudinalTuning", inline=False)
-    self.items = [self.longitudinal_tuning_item]
+
+    self.steer_max_item = option_item_sp(
+      title=lambda: tr("Max Steering Torque"),
+      min_val=100, max_val=400, value_chagne_step=5,
+      param="CustomSteerMax",
+      unit=""
+    )
+    self.steer_allowance_item = option_item_sp(
+      title=lambda: tr("CustomAllowance"),
+      min_val=50, max_val=250, value_chagne_step=50,
+      param="CustomAllowance",
+      unit=""
+    )
+    self.steer_threshold_item = option_item_sp(
+      title=lambda: tr("Steering Threshold"),
+      min_val=50, max_val=250, value_chagne_step=50,
+      param="CustomThreshold",
+      unit=""
+    )
+    self.steer_delta_up_item = option_item_sp(
+      title=lambda: tr("Steer Delta Up"),
+      min_val=1, max_val=4, value_chagne_step=1,
+      param="CustomDeltaUp",
+      unit=""
+    )
+    self.steer_delta_down_item = option_item_sp(
+      title=lambda: tr("Steer Delta Down"),
+      min_val=2, max_val=7, value_chagne_step=1,
+      param="CustomDeltaDown",
+      unit=""
+    )
+    self.items = [self.longitudinal_tuning_item, self.steer_max_item, self.steer_allowance_item,
+                  self.steer_delta_down_item, self.steer_delta_up_item, self.steer_threshold_item]
 
   @staticmethod
   def _on_tuning_selected(index):
@@ -51,6 +83,18 @@ class HyundaiSettings(BrandSettings):
         long_tuning_desc = tr("This feature is unavailable while the car is onroad.")
       elif not long_enabled:
         long_tuning_desc = tr("This feature is unavailable because sunnypilot Longitudinal Control (Alpha) is not enabled.")
+
+    # custom settings
+    self.steer_max_item.set_visible(True)
+    self.steer_allowance_item.set_visible(True)
+    self.steer_delta_down_item.set_visible(True)
+    self.steer_delta_up_item.set_visible(True)
+    self.steer_threshold_item.set_visible(True)
+    self.steer_max_item.set_enabled(True)
+    self.steer_allowance_item.set_enabled(True)
+    self.steer_delta_down_item.set_enabled(True)
+    self.steer_delta_up_item.set_enabled(True)
+    self.steer_threshold_item.set_enabled(True)
 
     self.longitudinal_tuning_item.action_item.set_enabled(not longitudinal_tuning_disabled)
     self.longitudinal_tuning_item.set_description(long_tuning_desc)
